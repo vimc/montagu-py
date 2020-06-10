fe6cb547ba721e6924836f9488e451e288110be3
Emma Russell <EmmaRussellKilburn@gmail.com>
2020-06-10 17:37:30 +0100

    Get token from montagu authenticate endpoint

--- src/montagu_api.py
+++ src/montagu_api.py
@@ -1,4 +1,17 @@
+import requests
+
+
 class MontaguAPI:
-    def __init__(self):
-        # TODO: take parameters and authenticate
-        s = "coming soon"
+    def __init__(self, base_url, username, password):
+        self.base_url = base_url
+        auth_url = self.build_url('authenticate/')
+        headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+        response = requests.post(auth_url, data='grant_type=client_credentials', auth=(username, password),
+                                 headers=headers)
+        if response.status_code != 200:
+            # TODO: throw error
+            print("ERROR: " + str(response.status_code))
+        self.token = response.json()['access_token']
+
+    def build_url(self, route):
+        return '{}/v1/{}'.format(self.base_url, route)
--- test/test_montagu_api.py
+++ test/test_montagu_api.py
@@ -2,4 +2,6 @@ from src.montagu_api import MontaguAPI
 
 
 def test_init():
-    api = MontaguAPI()
+    api = MontaguAPI('http://localhost:8080', 'test.user@example.com', 'password')
+    assert len(api.token) > 0
+    # TODO Get something innocuous from API to prove the token works!
